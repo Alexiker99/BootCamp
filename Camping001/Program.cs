5d05bdcb5c08a14acf790762b1c420a6b8d4d3fd
Alexiker99 <alexiker99@gmail.com>
2023-01-31 14:36:37 +0400

    Optimazed Code

--- Camping001/Program.cs
+++ Camping001/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.Diagnostics;
-// i v                     цикл по i v перебирает все элементы до элемента длинны - значения m
+// i           v             цикл по i v перебирает все элементы до элемента длинны - значения m
+//   - - - - - + + +
 //   0 1 2 3 4 5 6 7 8 9 ...........// Нумерация элементов 
 //   1 5 4 1 3 4 7 4 1 2 - пример
-// j ^ ^ ^
+// j           ^ ^ ^
 // Max -> 15 
 //
 //
@@ -15,18 +16,16 @@ int m = 3;
 int[] array = Enumerable.Range(1, size)
                         .Select(item => Random.Shared.Next(10)) //Генератор псевдослучайных чисел
                         .ToArray();
-//Console.WriteLine($"[{string.Join(", ", array)}]"); // Вывод элементов на экран
+Console.WriteLine($"[{string.Join(", ", array)}]"); // Вывод элементов на экран
 // Цикл на нахождение суммы 3 цифр в рандомном массиве
 Stopwatch sw = new(); // Создание компляра таймера
 sw.Start(); // Начало таймера
 int max = 0;
-for (int i = 0; i < array.Length - m; i++)
+for (int j = 0; j < m; j++) max += array[j];
+int t = max;
+for (int i = 1; i < array.Length - m; i++)
 {
-    int t = 0;
-    for (int j = i; j < i + m; j++)
-    {
-        t = t + array[j];
-    }
+    t = t - array[i - 1] + array [i + (m - 1)];
     if (t > max) max = t;
 }
 sw.Stop(); // Конец таймера
